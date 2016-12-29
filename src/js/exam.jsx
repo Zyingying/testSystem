80f17b1deaf42c52afa5447df8df7165b17d720d
Zyingying <494971645@qq.com>
2016-12-29 23:03:03 +0800

    去掉三级目录

--- src/js/exam.jsx
+++ src/js/exam.jsx
@@ -24,28 +24,22 @@ class exam extends React.Component {
             <div className="main-exam">
                 <Menu onClick={this.handleClick} style={{width:240}} mode="vertical">
                     <SubMenu key="sub1" title={<span><Icon type="mail" /><span>课内</span></span>}>
-                        <MenuItemGroup title="专业">
+                        {/*<MenuItemGroup title="专业">*/}
                             <Menu.Item key="1">算法</Menu.Item>
                             <Menu.Item key="2">计算机网络</Menu.Item>
-                        </MenuItemGroup>
-                        <MenuItemGroup title="通识">
+                        {/*</MenuItemGroup>*/}
+                        {/*<MenuItemGroup title="通识">*/}
                             <Menu.Item key="3">高等数学</Menu.Item>
                             <Menu.Item key="4">大学英语</Menu.Item>
-                        </MenuItemGroup>
+                        {/*</MenuItemGroup>*/}
                     </SubMenu>
-                    <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>导航二</span></span>}>
-                        <Menu.Item key="5">选项5</Menu.Item>
-                        <Menu.Item key="6">选项6</Menu.Item>
-                        <SubMenu key="sub3" title="三级导航">
-                            <Menu.Item key="7">选项7</Menu.Item>
-                            <Menu.Item key="8">选项8</Menu.Item>
-                        </SubMenu>
+                    <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>职业方向</span></span>}>
+                        <Menu.Item key="5">C++工程师</Menu.Item>
+                        <Menu.Item key="6">前端工程师</Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub4" title={<span><icon type="setting" /><span>导航三</span></span>}>
-                        <Menu.Item key="9">选项9</Menu.Item>
-                        <Menu.Item key="10">选项10</Menu.Item>
-                        <Menu.Item key="11">选项11</Menu.Item>
-                        <Menu.Item key="12">选项12</Menu.Item>
+                    <SubMenu key="sub4" title={<span><icon type="setting" /><span>时间</span></span>}>
+                        <Menu.Item key="9">2017</Menu.Item>
+                        <Menu.Item key="10">2016</Menu.Item>
                     </SubMenu>
                 </Menu>
 
