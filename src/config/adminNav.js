ac58e277a8af03b8815b298e1da10d5abc7dc223
Zyingying <494971645@qq.com>
2017-02-24 00:39:21 +0800

    后台内容配置项

---
+++ src/config/adminNav.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zyy on 2017/2/19.
+ */
+module.exports= [
+  {type:'change',content:'管理banner'},
+  {type:'change',content:'修改一级目录'},
+  {type:'change',content:'修改二级目录'},
+  {type:'change',content:'修改试题名称'},
+
+  {type:'creat',content:'添加一级目录'},
+  {type:'creat',content:'添加二级目录'},
+  {type:'creat',content:'添加试题名称'}
+
+];
