39ad6d63ac7cef87009f7395d3ddcfe1f349161d
Zyingying <494971645@qq.com>
2017-03-09 23:42:21 +0800

    个人中心列表

--- src/js/personal.jsx
+++ src/js/personal.jsx
@@ -1,16 +1,47 @@
 "use strict"
 const React = require("react");
 const Nav = require("../module/nav");
+import {Tabs,Button } from 'antd';
+const TabPane = Tabs.TabPane;
 
+function callback(key) {
+  console.log(key);
+}
 
 class Personal extends React.Component {
-    render() {
-        return <div className="f-page personal">
-                    <div className="w-categories">
-                        <Nav/>
-                    </div>
+  render() {
+    return <div className="f-page person">
+      <div className="w-categories">
+        <Nav/>
+      </div>
 
-                </div>;
-    }
+      <div className="main-person">
+        <div className="user">
+            <img src="" alt="" className="user-img"/>
+
+          <div>
+            <Button type="primary">返回首页</Button>
+            <Button type="danger">编辑资料</Button>
+          </div>
+        </div>
+
+        <Tabs defaultActiveKey="1"
+              onChange={callback}
+              className="show-person-list">
+          <TabPane tab="Tab 1" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+      </div>
+
+
+    </div>;
+  }
 }
 module.exports = Personal;
--- src/js/personalMsg.jsx
+++ src/js/personalMsg.jsx
@@ -1,56 +1,111 @@
 "use strict";
 const React = require("react");
 const Nav = require("../module/nav");
-import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button } from 'antd';
+import {Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, DatePicker,Radio} from 'antd';
 const FormItem = Form.Item;
 const Option = Select.Option;
+const RadioGroup = Radio.Group;
 
 const connectToStores = require("alt-utils/lib/connectToStores");
 const PerMsgAction = require("../action/perMsgAction");
 const PerMsgStore = require("../store/perMsgStore");
 
-class personMsg extends React.Component{
-    constructor(props){
-        super(props);
+class personMsg extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {}
+    this.state = {}
+  }
+
+  static getStores() {
+    return [PerMsgStore];
+  }
+
+  static getPropsFromStores() {
+    return {
+      ...PerMsgStore.getState()
     }
-    static getStores(){
-        return [PerMsgStore];
-    }
+  }
 
-    static getPropsFromStores(){
-        return {
-            ...PerMsgStore.getState()
-        }
-    }
+  handleSubmit() {
 
-    handleSubmit(){
-
-    }
+  }
 
 
+  render() {
 
-    render(){
+    return <div className="f-page personMsg" ref="personalMsg">
+      <div className="w-categories">
+        <Nav/>
+      </div>
 
-        return <div className="f-page personMsg" ref="personalMsg">
-            <div className="w-categories">
-                <Nav/>
-            </div>
+      <Form onSubmit={this.handleSubmit}
+            className="personMsg-Form"
+            layout="inline">
+        <FormItem label="账号邮箱"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}
+                  required="true">
+          <Input />
+        </FormItem>
+        <FormItem label="用户名"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}
+                  required="true">
+          <Input />
+        </FormItem>
+        <FormItem label="性别"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <RadioGroup>
+            <Radio value="a">保密</Radio>
+            <Radio value="b">男</Radio>
+            <Radio value="c">女</Radio>
+          </RadioGroup>
+        </FormItem>
+        <FormItem label="绑定手机"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <Input />
+        </FormItem>
+        <FormItem label="生日"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <DatePicker />
+        </FormItem>
 
-            <Form onSubmit={this.handleSubmit} className="personMsg-Form">
-                <FormItem
-                  label="E-mail"
-                  hasFeedback
-                >
-                      <Input />
-                    )}
-                </FormItem>
-            </Form>
+        <FormItem label="学校"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <Input />
+        </FormItem>
+        <FormItem label="学历"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <Select>
+            <Option value="中专">中专</Option>
+            <Option value="大专">大专</Option>
+            <Option value="本科">本科</Option>
+            <Option value="硕士">硕士</Option>
+            <Option value="博士">博士</Option>
+          </Select>
+        </FormItem>
+        <FormItem label="简介"
+                  labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <Input type="textarea" rows={4} />
+        </FormItem>
 
+        <FormItem labelCol={{span: 5}}
+                  wrapperCol={{span: 12}}>
+          <Button type="primary"
+                  htmlType="submit"
+                  className="change-sure">确认修改</Button>
+        </FormItem>
 
-        </div>
-    }
+      </Form>
+    </div>
+  }
 }
 
-module.exports =  connectToStores(personMsg);
+module.exports = connectToStores(personMsg);
